3ff2c37d905da1ef46e309906508bbcbc3b297dd
NguyenPhiThienn <phithien1007@@gmail.com>
2025-09-01 13:58:27 +0700

    Add project CDIDemo into Tuan2

---
+++ Tuan2/CDIDemo/src/main/java/iuh/fit/se/cdidemo/Hello.java
@@ -0,0 +1,13 @@
+package iuh.fit.se.cdidemo;
+
+import jakarta.inject.Inject;
+
+public class Hello {
+
+    @Inject
+    private World world;
+
+    public String helloWorld() {
+        return "Hello " + world.world();
+    }
+}
