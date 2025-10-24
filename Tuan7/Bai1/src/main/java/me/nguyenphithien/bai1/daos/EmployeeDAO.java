5b3edfcdc95c3bd6283f41a839acdd4913bc51a9
NguyenPhiThienn <phithien1007@@gmail.com>
2025-10-24 22:19:06 +0700

    tuan 7 8

---
+++ Tuan7/Bai1/src/main/java/me/nguyenphithien/bai1/daos/EmployeeDAO.java
@@ -0,0 +1,14 @@
+package me.nguyenphithien.bai1.daos;
+
+import me.nguyenphithien.bai1.entities.Employee;
+
+import java.util.List;
+
+
+public interface EmployeeDAO {
+    public void update (Employee employee);
+    public List<Employee> findAll ();
+    public Employee getById (int id);
+    public void deleteById (int id);
+    public void save (Employee employee);
+}
